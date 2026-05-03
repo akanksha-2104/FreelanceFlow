c6f5aa4f0016363976b98913cc10ff351d73580a
Akanksha Kawale <akankshaakawale@gmail.com>
2026-05-03 22:46:18 +0530

    Update API URL to use environment variable

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create Axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL || '/api',
 });
 
 // Request Interceptor → Attach token
